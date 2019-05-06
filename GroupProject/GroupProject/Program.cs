8b7d08934bffcddee2816bf22f4269263aa22480
Clayton Davidson (1000061387) <DAVICA3@student.op.ac.nz>
2019-05-06 13:08:16 +1200

    Fix missing folders from previous repo

---
+++ GroupProject/GroupProject/Program.cs
@@ -0,0 +1,127 @@
+﻿//5, 9, 3, 6
+using System;
+using System.IO;
+using System.Threading;
+
+namespace GroupProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            string job = "IT Support Person";
+            StreamReader sr = new StreamReader(Directory.GetCurrentDirectory() + @"\ITSupportPerson.txt");
+            string[] questions = sr.ReadLine().Split(','); //This will give us all our questions in an array
+            Intro(job);
+
+            do
+            {
+                Questions(sr);
+            } while (true);
+        }
+
+        static void Intro(string job) // Does the introductory sequence
+        {
+            Random rand = new Random();
+            string[] script = { "Hi, Thank you for coming in", $"I can see here you are applying for the IT Support Person position", "So lets get started" };
+            Face();
+            Console.Write("                             ");
+            foreach (string sentence in script)
+            {
+                foreach (char letter in sentence)
+                {
+                    Console.Write(letter);
+                    Thread.Sleep(rand.Next(30, 150)); //Changes the speed that letters appear to make it seem like a old school game
+                }
+                Thread.Sleep(1500);
+                Console.Clear();
+                Face();
+                Console.Write("                     ");
+            }
+        }
+
+        static void Face() //Face method to easily be called to print face on console
+        {
+            Console.WriteLine("                                XXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            Console.WriteLine("                              XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            Console.WriteLine("                             XXXXXXXXXXXXXXXXXX         XXXXXXXX");
+            Console.WriteLine("                            XXXXXXXXXXXXXXXX              XXXXXXX");
+            Console.WriteLine("                            XXXXXXXXXXXXX                   XXXXX");
+            Console.WriteLine("                             XXX     _________ _________     XXX");
+            Console.WriteLine("                              XX    I  _xxxxx I xxxxx_  I    XX  ");
+            Console.WriteLine("                             ( X----I         I         I----X ) ");
+            Console.WriteLine("                            ( +I    I      00 I 00      I    I+ )");
+            Console.WriteLine("                             ( I    I    __0  I  0__    I    I )");
+            Console.WriteLine(@"                              (I    I______ /   \_______I    I)");
+            Console.WriteLine("                               I           ( ___ )           I");
+            Console.WriteLine("                               I    _  :::::::::::::::  _    i");
+            Console.WriteLine(@"                                \    \___ ::::::::: ___/    /");
+            Console.WriteLine(@"                                \_      \_________/      _/");
+            Console.WriteLine(@"                                   \        \___,        /");
+            Console.WriteLine(@"                                     \                 /");
+            Console.WriteLine(@"                                      |\             /|");
+            Console.WriteLine(@"                                      |  \_________/  |");
+            Console.WriteLine("###############################################################################################");
+        }
+
+        static void Questions(StreamReader sr)
+        {
+            Random rand = new Random();
+            string reply;
+            string[] yes = { "yes", "sure", "okay", "definitely" };//Confirming answers
+            string[] maybe = { "maybe" };
+            string[] no = { "no", "never", "nope", "can't" };//Denying answers            foreach (string sentence in questions)
+            {
+                foreach (char letter in sentence)
+                {
+                    Console.Write(letter);
+                    Thread.Sleep(rand.Next(30, 150));
+                }
+                Console.Write("\n                     ");
+
+                reply = Console.ReadLine().ToLower(); // Stores reply to than be called to that questions own method
+
+                if (Array.BinarySearch(yes, reply) >= 0)//Exists in array
+                {
+                    Confirmation();//Passes in question number for appropriate follow up questions
+                }
+                else if (Array.BinarySearch(no, reply) >= 0)
+                {
+                    Denial();//Passes in question number for appropriate follow up questions
+                }
+                else if (Array.BinarySearch(maybe, reply) >= 0)
+                {
+                    Maybe();//Passes in question number for appropriate follow up questions
+                }
+                else
+                {
+                    Unknown();//Passes in question number for appropriate generic responses
+                }
+
+                Console.Clear();
+                Face();
+            }
+        }
+
+        static void Confirmation()
+        {
+
+        }
+
+        static void Denial()
+        {
+
+        }
+
+        static void Maybe()
+        {
+
+        }
+
+        static void Unknown()
+        {
+
+        }
+    }
+}
